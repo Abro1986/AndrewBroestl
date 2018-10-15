54a12e7b00ebb8c8cdd5108d5e6b8099449c33b9
Abro1986 <AndrewBroestl@gmail.com>
2018-10-15 14:54:37 -0600

    added a mailform ajax

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('working');
+let localHost = 'http:localhost:8080/api/mail'
+let deployed = 'https://fathomless-fortress-45044.herokuapp.com/api/mail'
+
+$('#mailform').submit(function (e) {
+
+	e.preventDefault();
+	$.ajax({
+		url: deployed,
+		type: 'POST',
+		data: $(this).serialize(),
+		success: function(data) {
+			$('#mailform').html(`<p class='text-white text-center'>${data}<p>`);
+		},
+		error: function (jXHR, textStatus, errorThrown) {
+                console.log(errorThrown);
+        },
+	})
+});
